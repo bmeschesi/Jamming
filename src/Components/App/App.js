77a74e5a331f5d8fd48f6471651a364fd31ebc2f
Bruno Meschesi <72936169+bmeschesi@users.noreply.github.com>
2020-12-13 14:01:24 +0000

    Edit playlist Name

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -15,6 +15,7 @@ class App extends React.Component {
     }
     this.addTrack = this.addTrack.bind(this);
     this.removeTrack = this.removeTrack.bind(this);
+    this.updatePlaylistName = this.updatePlaylistName.bind(this);
   }
 
   addTrack(track) {
@@ -34,6 +35,10 @@ class App extends React.Component {
     this.setState({ playlistTracks: tracks });
   }
 
+  updatePlaylistName(name) {
+    this.setState({ playlistName: name })
+  }
+
   render() {
     return (
       < div >
@@ -44,7 +49,8 @@ class App extends React.Component {
             <SearchResults searchResults={this.state.searchResults} onAdd={this.addTrack} />
             <Playlist playlistName={this.state.playlistName}
               playlistTracks={this.state.playlistTracks}
-              onRemove={this.removeTrack} />
+              onRemove={this.removeTrack} 
+              onNameChange={this.updatePlaylistName} />
           </div>
         </div>
       </div >
