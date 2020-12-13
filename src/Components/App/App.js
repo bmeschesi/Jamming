e265aaf2e58dda6059363ed00e03181b27041b88
Bruno Meschesi <72936169+bmeschesi@users.noreply.github.com>
2020-12-13 12:14:27 +0000

    Added Track to Playlist

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -5,12 +5,24 @@ import SearchResults from '../SearchResults/SearchResults';
 import Playlist from '../Playlist/Playlist';
 
 class App extends React.Component {
+
   constructor(props) {
     super(props);
     this.state = {
       searchResults: [{ name: 'name1', artist: 'artist1', album: 'album1', id: 1 }, { name: 'name2', artist: 'artist2', album: 'album2', id: 2 }, { name: 'name3', artist: 'artist3', album: 'album3', id: 3 }],
       playlistName: 'My Playlist',
       playlistTracks: [{ name: 'playlistName1', artist: 'playlistArtist1', album: 'playListAlbum1', id: 4 }, { name: 'playlistName2', artist: 'playlistArtist2', album: 'playListAlbum2', id: 5 }, { name: 'playlistName3', artist: 'playlistArtist3', album: 'playListAlbum3', id: 6 }]
+    }
+    this.addTrack = this.addTrack.bind(this);
+  }
+
+  addTrack(track) {
+    let tracks = this.state.playlistTracks;
+    if (tracks.find(savedTrack => savedTrack.id === track.id)) {
+      return;
+    } else {
+      tracks.push(track);
+      this.setState({ playlistTracks: tracks })
     }
   }
 
@@ -21,7 +33,7 @@ class App extends React.Component {
         <div className="App">
           <SearchBar />
           <div className="App-playlist">
-            <SearchResults searchResults={this.state.searchResults} />
+            <SearchResults searchResults={this.state.searchResults} onAdd={this.addTrack} />
             <Playlist playlistName={this.state.playlistName}
               playlistTracks={this.state.playlistTracks} />
           </div>
--- src/Components/Track/Track.js
+++ src/Components/Track/Track.js
@@ -3,20 +3,30 @@ import React from 'react';
 import './Track.css';
 
 class Track extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.addTrack = this.addTrack.bind(this);
+}
+
     renderAction() {
         if (this.props.isremoval) {
             return <button className="Track-action">-</button>
         } else {
-            return <button className="Track-action">+</button>
+            return <button className="Track-action" onClick={this.addTrack}>+</button>
         }
+    }
+
+    addTrack() {
+        this.props.onAdd(this.props.track);
     }
 
     render() {
         return (
             <div className="Track">
                 <div className="Track-information">
-        <h3>{this.props.track.name}</h3>
-        <p>{this.props.track.artist} | {this.props.track.name}</p>
+                    <h3>{this.props.track.name}</h3>
+                    <p>{this.props.track.artist} | {this.props.track.name}</p>
                 </div>
                 {this.renderAction()}
             </div>
